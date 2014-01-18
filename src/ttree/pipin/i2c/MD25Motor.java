9166ff79cb2652ab6daa19fce6f2581b66b16607
michael <mail@michael-stevens.de>
2014-01-19 00:33:00 +0100

    MD25 encoder reading

--- src/ttree/pipin/i2c/MD25Motor.java
+++ src/ttree/pipin/i2c/MD25Motor.java
@@ -3,6 +3,7 @@ package ttree.pipin.i2c;
 import java.io.IOException;
 
 import com.pi4j.io.i2c.I2CDevice;
+import static ttree.pipin.i2c.MD25.*;
 
 /**
  * Dual Motor control via MD25
@@ -24,7 +25,7 @@ public final class MD25Motor {
 
 		this.device = device;
 		
-		int actualRevision = device.read(MD25.REG_SOFTWARE_REVISION);
+		int actualRevision = device.read(REG_SOFTWARE_REVISION);
 		if (actualRevision <= revision) {
 			throw new IllegalStateException("MD25 actual revision: " + actualRevision + " below required revision:" + revision);
 		}
@@ -32,7 +33,7 @@ public final class MD25Motor {
 		if (mode < 0 || mode > 3) {
 			throw new IllegalArgumentException("MD25 mode must be 0..3");
 		}
-		device.write(MD25.REG_MODE, mode);
+		device.write(REG_MODE, mode);
 	}
 
 	/**
@@ -40,7 +41,7 @@ public final class MD25Motor {
 	 */
 	public void keepAlive() throws IOException {
 
-		device.read(MD25.REG_MODE);
+		device.read(REG_MODE);
 	}
 	
 	/**
@@ -50,7 +51,7 @@ public final class MD25Motor {
 	 */
 	public void setSpeed1(byte speed) throws IOException {
 
-		device.write(MD25.REG_SPEED1, speed);
+		device.write(REG_SPEED1, speed);
 	}
 	
 	/**
@@ -60,7 +61,33 @@ public final class MD25Motor {
 	 */
 	public void setSpeed2(byte speed) throws IOException {
 
-		device.write(MD25.REG_SPEED2, speed);
+		device.write(REG_SPEED2, speed);
 	}
 	
+	/**
+	 * Encoder for the motor 1
+	 * @return encoder count
+	 * @throws IOException
+	 */
+	public int encoder1() throws IOException {
+		int enc1a = device.read(REG_ENC1A);	// capture encoder
+		int enc1b = device.read(REG_ENC1B);
+		int enc1c = device.read(REG_ENC1C);
+		int enc1d = device.read(REG_ENC1D);
+		return ((enc1a << 8 | enc1b) << 8 | enc1c) << 8 | enc1d;
+	}
+
+	/**
+	 * Encoder for the motor 2
+	 * @return encoder count
+	 * @throws IOException
+	 */
+	public int encoder2() throws IOException {
+		int enc2a = device.read(REG_ENC2A);	// capture encoder
+		int enc2b = device.read(REG_ENC2B);
+		int enc2c = device.read(REG_ENC2C);
+		int enc2d = device.read(REG_ENC2D);
+		return ((enc2a << 8 | enc2b) << 8 | enc2c) << 8 | enc2d;
+	}
+
 }

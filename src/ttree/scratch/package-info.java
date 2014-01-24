488cd3fd6aea2b7142c6e82f3a2d121b48eadbd6
michael <mail@michael-stevens.de>
2014-01-24 12:38:35 +0100

    ADD package info

---
+++ src/ttree/scratch/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Scratch remote sensor interfacing
+ * 
+ * Scratch provides a socket based remote sensor protocol. This can be used to
+ * connect external devices for sensing and output.
+ * 
+ * Copyright Michael Stevens 2013
+ */
+package ttree.scratch;

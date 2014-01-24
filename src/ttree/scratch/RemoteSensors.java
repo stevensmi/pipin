d40c4d58f3ce00b8f36095882e39dc133c214b5a
michael <mail@michael-stevens.de>
2014-01-24 23:14:23 +0100

    ScratchRobo as an example of how to use the two sensors

---
+++ src/ttree/scratch/RemoteSensors.java
@@ -0,0 +1,114 @@
+package ttree.scratch;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+
+import ttree.scratch.protocol.ScratchConnection;
+import ttree.scratch.protocol.ScratchRemoteProtocol;
+
+/*
+ * Scratch remote sensors
+ *
+ * Provider support for multiple remote sensors with a single scratch connection
+ */
+public class RemoteSensors implements IncomingMessage, Runnable {
+
+	final static Logger log = Logger.getLogger("RemoteSensors");
+
+	final private ScratchConnection scratchRemote;
+	final private List<IncomingMessage> remoteSensors;
+
+	/**
+	 * Construct with list of remote sensors to use with scratch
+	 * @param scratchRemote
+	 * @param remoteSensors remote sensors for incoming messages
+	 * @throws IOException
+	 */
+	public RemoteSensors(ScratchConnection scratchRemote, List<IncomingMessage> remoteSensors) {
+		
+		this.scratchRemote = scratchRemote;
+		this.remoteSensors = remoteSensors;
+	}
+	
+	/**
+	 * Read incoming messages from scratch and pass it on to the remote sensors
+	 */
+	@Override
+	public void run() {
+		final ScratchRemoteProtocol command = new ScratchRemoteProtocol();
+		for (;;) {
+			String line;
+			try {
+				line = scratchRemote.readLine();
+			} catch (IOException e) {
+				log.warning("Connection to scratch remote sensor lost: " + e.getMessage());
+				break;
+			}
+			if (line == null) {
+				break;
+			}
+	
+			command.parse(line, this);
+		}
+		log.info("scratch finished");
+	}
+
+
+	@Override
+	public void broadcast(String message) {
+		
+		// simply let all the remote sensors handle the broadcast in turn
+		for (IncomingMessage remote : remoteSensors) {
+			remote.broadcast(message);
+		}
+	}
+
+	@Override
+	public void sensorUpdate(String name, String value) {
+
+		// simply let all the remote sensors handle the sensor update in turn
+		for (IncomingMessage remote : remoteSensors) {
+			remote.sensorUpdate(name, value);
+		}
+	}
+
+	/**
+	 * Provide support for the outgoing messages 
+	 *
+	 */
+	public static class OutgoingMessages implements OutgoingMessage {
+
+		private final ScratchConnection scratchRemote;
+		
+		public OutgoingMessages(ScratchConnection scratchRemote) {
+			this.scratchRemote = scratchRemote;
+		}
+
+		final ScratchRemoteProtocol remoteProtocol = new ScratchRemoteProtocol();
+		
+		@Override
+		public synchronized void broadcast(String message) {
+			final String broadcastLine = remoteProtocol.generateBroadcast(message);
+			try {
+				scratchRemote.writeLine(broadcastLine);
+			} catch (IOException e) {
+				log.warning("Scratch remote not recieving messages");
+			}
+			
+		}
+
+		@Override
+		public synchronized void sensorUpdate(String... updates) {
+			final String updateLine = remoteProtocol.generateSensorUpdate(updates);
+			try {
+				scratchRemote.writeLine(updateLine);
+			} catch (IOException e) {
+				log.warning("Scratch remote not recieving messages");
+			}
+		}
+		
+	}
+
+
+}
--- src/ttree/scratch/ScratchRobo.java
+++ src/ttree/scratch/ScratchRobo.java
@@ -5,9 +5,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import ttree.scratch.RemoteSensors.OutgoingMessages;
 import ttree.scratch.md25.MD25Factory;
 import ttree.scratch.protocol.ScratchConnection;
-import ttree.scratch.protocol.ScratchRemoteProtocol;
 import ttree.scratch.tcl59116.TCL59116Factory;
 
 import com.pi4j.io.i2c.I2CBus;
@@ -15,16 +15,12 @@ import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.io.i2c.I2CFactory;
 
 /*
- * Scratch remote for robot control
+ * Scratch remote for robot control with a MD25 motor controller and an TCL59116 KED driver
  */
-public class ScratchRobo implements IncomingMessage {
+public class ScratchRobo  {
 
 	final static Logger log = Logger.getLogger("ScratchRobo");
 
-	private ScratchConnection scratchRemote;
-	
-	final private List<IncomingMessage> remoteSensors = new LinkedList<IncomingMessage>();
-	
 	/**
 	 * Main for Scratch remote sensor support
 	 */
@@ -57,100 +53,39 @@ public class ScratchRobo implements IncomingMessage {
 			return;
 		}
 
+		// build remoteSensors from the command line 
+		final List<IncomingMessage> remoteSensors = new LinkedList<IncomingMessage>();
+		
 		// scratch remote parse and sensor output
-		final ScratchRobo self;
 		try {
-			self = new ScratchRobo(address_md25, address_tcl59116);
+			// RPi external I2C bus
+			final I2CBus piExtBus = I2CFactory.getInstance(1);
+
+			// Devices
+			final I2CDevice device_md25 = piExtBus.getDevice(address_md25);
+			final I2CDevice device_tcl59116 = piExtBus.getDevice(address_tcl59116);
+
+			final OutgoingMessages messageHandler = new OutgoingMessages(scratchRemote);
+			
+			final IncomingMessage md25 = new MD25Factory(1).make(messageHandler, device_md25);
+			if (md25 != null) {
+				remoteSensors.add(md25);
+			}
+
+			final IncomingMessage leds = new TCL59116Factory(1).make(messageHandler, device_tcl59116);
+			if (leds != null) {
+				remoteSensors.add(leds);
+			}
+
 		} catch (IOException e) {
 			log.warning("Cannot connect to i2c sensor: " + e.getMessage());
 			System.exit(2);	// signal no connection to i2c
 			return;
 		}
-		self.scratchRemote = scratchRemote;
-
-		final ScratchRemoteProtocol command = new ScratchRemoteProtocol();
-		for (;;) {
-			String line;
-			try {
-				line = scratchRemote.readLine();
-			} catch (IOException e) {
-				log.warning("Connection to scratch remote sensor lost: " + e.getMessage());
-				break;
-			}
-			if (line == null) {
-				break;
-			}
-
-			command.parse(line, self);
-		}
-		log.info("scratch finished");
-	}
-
-	public ScratchRobo(int address_md25, int address_tcl59116) throws IOException {
-		// RPi external I2C bus
-		final I2CBus piExtBus = I2CFactory.getInstance(1);
-
-		// Devices
-		final I2CDevice device_md25 = piExtBus.getDevice(address_md25);
-		final I2CDevice device_tcl59116 = piExtBus.getDevice(address_tcl59116);
-
-		final OutgoingMessages messageHandler = new OutgoingMessages();
 		
-		final IncomingMessage md25 = new MD25Factory(1).make(messageHandler, device_md25);
-		if (md25 != null) {
-			remoteSensors.add(md25);
-		}
-
-		final IncomingMessage leds = new TCL59116Factory(1).make(messageHandler, device_tcl59116);
-		if (leds != null) {
-			remoteSensors.add(leds);
-		}
+		// start handling incoming remote messages
+		final RemoteSensors remoteRunner = new RemoteSensors(scratchRemote, remoteSensors);
+		remoteRunner.run();
 	}
-
-	@Override
-	public void broadcast(String message) {
-		
-		// simply let all the remote sensors handle the broadcast in turn
-		for (IncomingMessage remote : remoteSensors) {
-			remote.broadcast(message);
-		}
-	}
-
-	@Override
-	public void sensorUpdate(String name, String value) {
-
-		// simply let all the remote sensors handle the sensor update in turn
-		for (IncomingMessage remote : remoteSensors) {
-			remote.sensorUpdate(name, value);
-		}
-	}
-
-	class OutgoingMessages implements OutgoingMessage {
-
-		final ScratchRemoteProtocol remoteProtocol = new ScratchRemoteProtocol();
-		
-		@Override
-		public synchronized void broadcast(String message) {
-			final String broadcastLine = remoteProtocol.generateBroadcast(message);
-			try {
-				scratchRemote.writeLine(broadcastLine);
-			} catch (IOException e) {
-				log.warning("Scratch remote not recieving messages");
-			}
-			
-		}
-
-		@Override
-		public synchronized void sensorUpdate(String... updates) {
-			final String updateLine = remoteProtocol.generateSensorUpdate(updates);
-			try {
-				scratchRemote.writeLine(updateLine);
-			} catch (IOException e) {
-				log.warning("Scratch remote not recieving messages");
-			}
-		}
-		
-	}
-
 
 }
